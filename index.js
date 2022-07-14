ab6d44590b1cd1c5e4fb3e01ec6575df07194a03
Daniel Eduardo Campos <daniele.campos@ingenico.com>
2022-07-13 20:08:51 -0500

    first commit create functions for users & products

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/userRoutes.js'
+import seedRouter from './routes/seedRoutes.js'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('connected to Mongo DB');
+}).catch((err) => {
+    console.log(err.message)
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/seed', seedRouter);
+app.use('/api/users/', userRouter)
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`)
+});
